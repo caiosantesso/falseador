c898b0e5513461f673c9f6a77c30b0b66926a808
Caio Santesso <>
2023-03-21 22:12:11 -0300

    remove linha de comando

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,4 @@
 #!/usr/bin/env node
 import { Falseador } from './Falseador';
-import { LinhaDeComando } from './LinhaDeComando';
 
 export const falseador = new Falseador();
-
-const foiExecutadoDoTerminal = require.main === module;
-
-if (foiExecutadoDoTerminal) {
-  const linhaDeComando = new LinhaDeComando(falseador);
-  linhaDeComando.execute();
-}
